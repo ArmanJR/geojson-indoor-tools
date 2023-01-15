9481168ec424c6c35db3b955c796f47c4bb79f42
Arman <armanjafarnejad@gmail.com>
2023-01-16 00:14:57 +0330

    rooms to walls complete refactor

--- rooms-to-walls.py
+++ rooms-to-walls.py
@@ -1,40 +1,78 @@
-from turfpy.transformation import transform_scale
-from geojson import Polygon, Feature
 import json
+from geojson import Feature, LineString, FeatureCollection, Polygon, MultiLineString
+from turfpy.transformation import line_offset, transform_scale, union
+import warnings
 
-zeta = 0.05
+warnings.filterwarnings('ignore')  # for turfpy warnings
+zeta = 0.07  # walls thickness of polygons
+walls_thickness = 1.04  # walls thickness of non-polygons
+walls_properties = {'height': 5, 'color': 'green'}
+include_original = False
+
+
+def polygon_walls(poly):
+    f = Feature(geometry=Polygon(poly['geometry']['coordinates']))
+    outer = transform_scale(f, 1 + zeta, origin='centroid')
+    inner = transform_scale(f, 1 - zeta, origin='centroid')
+
+    walls_coords = inner['geometry']['coordinates'][0] + outer['geometry']['coordinates'][0] + [
+        inner['geometry']['coordinates'][0][0]]
+
+    walls_feature = {
+        'type': 'Feature', 'properties': walls_properties, 'geometry': {
+            'type': 'Polygon',
+            'coordinates': [walls_coords]
+        }
+    }
+    return walls_feature
+
+
+def line_string_walls(line_string):
+    offset_top = line_offset(line_string['geometry'], distance=walls_thickness, unit='m')
+    offset_bot = line_offset(line_string['geometry'], distance=-walls_thickness, unit='m')
+    merged_coords = [
+        offset_top['geometry']['coordinates'] +
+        list(reversed(offset_bot['geometry']['coordinates'])) +
+        [offset_top['geometry']['coordinates'][0]]
+    ]
+    return Feature(geometry=Polygon(coordinates=merged_coords), properties=walls_properties)
+
+
+def multi_line_string_walls(multi_line_string):
+    polys = []
+    for line_string_coords in multi_line_string['geometry']['coordinates']:
+        line_string = Feature(geometry=LineString(coordinates=line_string_coords))
+        poly = line_string_walls(line_string)
+        polys.append(poly)
+
+    unions = union(FeatureCollection(polys))
+    unions['properties'] = walls_properties
+    return unions
+
 
 f = open('data/rooms-to-walls/input.geojson', encoding='utf-8')
 data = json.load(f)
 
 wallsFeatures = []
 for feature in data['features']:
-    if feature['geometry']['type'] != 'Polygon':
+    featureType = feature['geometry']['type']
+
+    if include_original:
         wallsFeatures.append(feature)
-        continue
 
-    f = Feature(geometry=Polygon(feature['geometry']['coordinates']))
-    outer = transform_scale(f, 1 + zeta, origin='centroid')
-    inner = transform_scale(f, 1 - zeta, origin='centroid')
-
-    walls = inner['geometry']['coordinates'][0] + outer['geometry']['coordinates'][0] + [
-        inner['geometry']['coordinates'][0][0]]
-
-    wallsFeature = {
-        'type': 'Feature', 'properties': {
-            'height': 5,
-            'color': 'green'
-        }, 'geometry': {
-            'type': 'Polygon',
-            'coordinates': [walls]
-        }
-    }
-
-    wallsFeatures.append(wallsFeature)
+    if featureType == 'Polygon':
+        walls = polygon_walls(feature)
+        wallsFeatures.append(walls)
+    elif featureType == 'LineString':
+        walls = line_string_walls(feature)
+        wallsFeatures.append(walls)
+    elif featureType == 'MultiLineString':
+        walls = multi_line_string_walls(feature)
+        wallsFeatures.append(walls)
 
 newGeojson = {'type': 'FeatureCollection', 'features': wallsFeatures}
 
 with open('data/rooms-to-walls/output.geojson', 'w', encoding='utf-8') as f:
-    json.dump(newGeojson, f)
+    json.dump(newGeojson, f, indent=4)
 
 f.close()
